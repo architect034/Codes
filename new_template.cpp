1269af66ed921a8c8a976323df1c8622074a4620
architect034 <you@example.com>
2020-04-20 06:55:35 +0530

    committing again

--- new_template.cpp
+++ new_template.cpp
@@ -20,21 +20,27 @@ const int MAX = 1e5 + 9;
 const ll mod = 1e9 + 7;
 vector<bool> prime(MAX, 1);
 vector<int> spf(MAX, 1), primes;
-void sieve() {
+void sieve()
+{
    prime[0] = prime[1] = 0;
    spf[2] = 2;
-   for (ll i = 4; i < MAX; i += 2) {
+   for (ll i = 4; i < MAX; i += 2)
+   {
       spf[i] = 2;
       prime[i] = 0;
    }
    primes.pb(2);
-   for (ll i = 3; i < MAX; i += 2) {
-      if (prime[i]) {
+   for (ll i = 3; i < MAX; i += 2)
+   {
+      if (prime[i])
+      {
          primes.pb(i);
          spf[i] = i;
-         for (ll j = i * i; j < MAX; j += i) {
+         for (ll j = i * i; j < MAX; j += i)
+         {
             prime[j] = 0;
-            if (spf[j] == 1) {
+            if (spf[j] == 1)
+            {
                spf[j] = i;
             }
          }
@@ -42,41 +48,52 @@ void sieve() {
    }
 }
 template <typename T>
-void in(T &x) {
+void in(T &x)
+{
    cin >> x;
 }
 template <typename T, typename U>
-void in(T &x, U &y) {
+void in(T &x, U &y)
+{
    cin >> x >> y;
 }
 template <typename T, typename U, typename V>
-void in(T &x, U &y, V &z) {
+void in(T &x, U &y, V &z)
+{
    cin >> x >> y >> z;
 }
 template <typename T>
-void ps(T x) {
+void ps(T x)
+{
    cout << x << " ";
 }
 template <typename T>
-void ps(const vector<T> &x, int n) {
-   f(i, 0, n) {
+void ps(const vector<T> &x, int n)
+{
+   f(i, 0, n)
+   {
       cout << x[i];
       (i == n - 1) ? cout << endl : cout << " ";
    }
 }
 template <typename T>
-void pl(T x) {
+void pl(T x)
+{
    cout << x << endl;
 }
 template <typename T>
-void pl(const vector<T> &x, int n) {
+void pl(const vector<T> &x, int n)
+{
    f(i, 0, n) { cout << x[i] << "\n"; }
 }
 template <typename T>
-T power(T a, T b) {
+T power(T a, T b)
+{
    T res = 1;
-   while (b) {
-      if (b & 1) {
+   while (b)
+   {
+      if (b & 1)
+      {
          res = res * a;
       }
       a = a * a;
@@ -85,10 +102,13 @@ T power(T a, T b) {
    return res;
 }
 template <typename T>
-T power(T a, T b, T m) {
+T power(T a, T b, T m)
+{
    ll res = 1;
-   while (b) {
-      if (b & 1) {
+   while (b)
+   {
+      if (b & 1)
+      {
          res = (res * a) % m;
       }
       a = (a * a) % m;
@@ -96,19 +116,24 @@ T power(T a, T b, T m) {
    }
    return res % m;
 }
-void virtual_main() {
+void virtual_main()
+{
 #ifndef ONLINE_JUDGE
    freopen("input.txt", "r", stdin);
    freopen("output.txt", "w", stdout);
 #endif
 }
-void real_main() {}
-signed main() {
+void real_main()
+{
+}
+signed main()
+{
    Fast;
    virtual_main();
    int test_cases = 1;
    // cin >> test_cases;
-   for (int i = 1; i <= test_cases; i++) {
+   for (int i = 1; i <= test_cases; i++)
+   {
       // cout << "Case #" << tc << ": ";
       real_main();
    }
