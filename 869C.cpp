7c4a06d8959948d6d1818065ecde6e096014d783
architect034 <you@example.com>
2020-04-29 01:06:46 +0530

    29 april 2020

---
+++ 869C.cpp
@@ -0,0 +1,146 @@
+#include <bits/stdc++.h>
+#define Fast ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define popb pop_back
+#define popf pop_front
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl '\n'
+#define all(something) something.begin(), something.end()
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define nl cout << endl
+using namespace std;
+const int MAX = 5e3 + 9;
+const ll mod = 998244353;
+vector<bool> prime(MAX, 1);
+vector<int> spf(MAX, 1), primes;
+void sieve() {
+   prime[0] = prime[1] = 0;
+   spf[2] = 2;
+   for (ll i = 4; i < MAX; i += 2) {
+      spf[i] = 2;
+      prime[i] = 0;
+   }
+   primes.pb(2);
+   for (ll i = 3; i < MAX; i += 2) {
+      if (prime[i]) {
+         primes.pb(i);
+         spf[i] = i;
+         for (ll j = i * i; j < MAX; j += i) {
+            prime[j] = 0;
+            if (spf[j] == 1) {
+               spf[j] = i;
+            }
+         }
+      }
+   }
+}
+template <typename T>
+void in(T &x) {
+   cin >> x;
+}
+template <typename T, typename U>
+void in(T &x, U &y) {
+   cin >> x >> y;
+}
+template <typename T, typename U, typename V>
+void in(T &x, U &y, V &z) {
+   cin >> x >> y >> z;
+}
+template <typename T>
+void ps(T x) {
+   cout << x << " ";
+}
+template <typename T>
+void ps(const vector<T> &x, int n) {
+   for (int i = 0; i < n; i++) {
+      cout << x[i];
+      (i == n - 1) ? cout << endl : cout << " ";
+   }
+}
+template <typename T>
+void pl(T x) {
+   cout << x << endl;
+}
+template <typename T>
+void pl(const vector<T> &x, int n) {
+   for (int i = 0; i < n; i++) {
+      cout << x[i] << "\n";
+   }
+}
+template <typename T>
+T power(T a, T b) {
+   T res = 1;
+   while (b) {
+      if (b & 1) {
+         res = res * a;
+      }
+      a = a * a;
+      b = b >> 1;
+   }
+   return res;
+}
+template <typename T>
+T power(T a, T b, T m) {
+   T res = 1;
+   while (b) {
+      if (b & 1) {
+         res = (res * a) % m;
+      }
+      a = (a * a) % m;
+      b = b >> 1;
+   }
+   return res % m;
+}
+#define int long long
+int fact[MAX];
+void virtual_main() {
+#ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+#endif
+   fact[0] = 1;
+   for (int i = 1; i < MAX; i++) {
+      fact[i] = i * fact[i - 1];
+      fact[i] %= mod;
+   }
+}
+int ncr(int n, int r) {
+   int res = 1;
+   res = res * fact[n];
+   res %= mod;
+   res = res * power(fact[r], mod - 2, mod);
+   res %= mod;
+   res = res * power(fact[n - r], mod - 2, mod);
+   res %= mod;
+   return res;
+}
+int solve(int x, int y) {
+   int ans = 0;
+   for (int i = 0; i <= min(x, y); i++) {
+      ans += (((fact[i] * ncr(x, i)) % mod) * ncr(y, i)) % mod;
+      ans = ans % mod;
+   }
+   return ans;
+}
+
+void real_main() {
+   int a, b, c;
+   cin >> a >> b >> c;
+   cout << (((solve(a, b) * solve(b, c)) % mod) * solve(c, a)) % mod;
+}
+signed main() {
+   Fast;
+   virtual_main();
+   int test_cases = 1;
+   // cin >> test_cases;
+   for (int i = 1; i <= test_cases; i++) {
+      // cout << "Case #" << tc << ": ";
+      real_main();
+   }
+   return 0;
+}
