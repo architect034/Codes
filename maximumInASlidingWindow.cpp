70ef61b94dedfd8f37f8835bec695d38b8964385
architect034 <gobindsingh034@gmail.com>
2020-08-02 16:34:01 +0530

    2 august 2020

---
+++ maximumInASlidingWindow.cpp
@@ -0,0 +1,145 @@
+#define dbg(...) ;
+#define db(...) ;
+#include "bits/stdc++.h"
+#define IOE ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL), cerr.tie(NULL)
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define popb pop_back
+#define popf pop_front
+#define ff first
+#define ss second
+#define endl '\n'
+#define all(something) something.begin(), something.end()
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define nl cout << endl
+#define PI 3.14159265358979323846
+using namespace std;
+const int MAX = 2e5 + 9;
+const ll mod = 1e9 + 7;
+vector<bool> prime(MAX, 1);
+vector<int> spf(MAX, 1), primes;
+void sieve() {
+   prime[0] = prime[1] = 0;
+   spf[2] = 2;
+   for (ll i = 4; i < MAX; i += 2) {
+      spf[i] = 2;
+      prime[i] = 0;
+   }
+   primes.pb(2);
+   for (ll i = 3; i < MAX; i += 2) {
+      if (prime[i]) {
+         primes.pb(i);
+         spf[i] = i;
+         for (ll j = i * i; j < MAX; j += i) {
+            prime[j] = 0;
+            if (spf[j] == 1) {
+               spf[j] = i;
+            }
+         }
+      }
+   }
+}
+template <typename T>
+void ps(T x) {
+   cout << x << " ";
+}
+template <typename T>
+void ps(const vector<T> &x, int n) {
+   for (int i = 0; i < n; i++) {
+      cout << x[i];
+      (i == n - 1) ? cout << endl : cout << " ";
+   }
+}
+template <typename T>
+void pl(T x) {
+   cout << x << endl;
+}
+template <typename T>
+void pl(const vector<T> &x, int n) {
+   for (int i = 0; i < n; i++) {
+      cout << x[i] << "\n";
+   }
+}
+template <typename T>
+T power(T a, T b) {
+   T res = 1;
+   while (b) {
+      if (b & 1) {
+         res = res * a;
+      }
+      a = a * a;
+      b = b >> 1;
+   }
+   return res;
+}
+template <typename T>
+T power(T a, T b, T m) {
+   T res = 1;
+   while (b) {
+      if (b & 1) {
+         res = (res * a) % m;
+      }
+      a = (a * a) % m;
+      b = b >> 1;
+   }
+   return res % m;
+}
+void _IOE() {
+#ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+   freopen("error.txt", "w", stderr);
+#endif
+}
+// #define int long long
+class Solution {
+  public:
+   vector<int> maxSlidingWindow(vector<int> &v, int k) {
+      int n = v.size();
+      vector<int> ans;
+      if (n < k) {
+         return ans;
+      }
+      deque<int> dq;
+      for (int i = 0; i < k; i++) {
+         while (dq.size() && v[dq.back()] <= v[i]) {
+            dq.pop_back();
+         }
+         dq.push_back(i);
+      }
+      ans.push_back(v[dq.front()]);
+      for (int i = k; i < n; i++) {
+         while (dq.size() && dq.front() <= (i - k)) {
+            dq.pop_front();
+         }
+         while (dq.size() && v[dq.back()] <= v[i]) {
+            dq.pop_back();
+         }
+         dq.push_back(i);
+         ans.push_back(v[dq.front()]);
+      }
+      return ans;
+   }
+   void solution() {
+      int n;
+      cin >> n;
+      vector<int> v(n);
+      for (int &i : v) cin >> i;
+      int k;
+      cin >> k;
+      this->maxSlidingWindow(v, k);
+   }
+};
+signed main() {
+   IOE;
+   _IOE();
+   int test_cases = 1;
+   // cin >> test_cases;
+   for (int i = 1; i <= test_cases; i++) {
+      Solution obj;
+      obj.solution();
+   }
+   return 0;
+}
---
+++ minimumWindowSubstring.cpp
@@ -0,0 +1,150 @@
+#define dbg(...) ;
+#define db(...) ;
+#include "bits/stdc++.h"
+#define IOE ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL), cerr.tie(NULL)
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define popb pop_back
+#define popf pop_front
+#define ff first
+#define ss second
+#define endl '\n'
+#define all(something) something.begin(), something.end()
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define nl cout << endl
+#define PI 3.14159265358979323846
+using namespace std;
+const int MAX = 2e5 + 9;
+const ll mod = 1e9 + 7;
+vector<bool> prime(MAX, 1);
+vector<int> spf(MAX, 1), primes;
+void sieve() {
+   prime[0] = prime[1] = 0;
+   spf[2] = 2;
+   for (ll i = 4; i < MAX; i += 2) {
+      spf[i] = 2;
+      prime[i] = 0;
+   }
+   primes.pb(2);
+   for (ll i = 3; i < MAX; i += 2) {
+      if (prime[i]) {
+         primes.pb(i);
+         spf[i] = i;
+         for (ll j = i * i; j < MAX; j += i) {
+            prime[j] = 0;
+            if (spf[j] == 1) {
+               spf[j] = i;
+            }
+         }
+      }
+   }
+}
+template <typename T>
+void ps(T x) {
+   cout << x << " ";
+}
+template <typename T>
+void ps(const vector<T> &x, int n) {
+   for (int i = 0; i < n; i++) {
+      cout << x[i];
+      (i == n - 1) ? cout << endl : cout << " ";
+   }
+}
+template <typename T>
+void pl(T x) {
+   cout << x << endl;
+}
+template <typename T>
+void pl(const vector<T> &x, int n) {
+   for (int i = 0; i < n; i++) {
+      cout << x[i] << "\n";
+   }
+}
+template <typename T>
+T power(T a, T b) {
+   T res = 1;
+   while (b) {
+      if (b & 1) {
+         res = res * a;
+      }
+      a = a * a;
+      b = b >> 1;
+   }
+   return res;
+}
+template <typename T>
+T power(T a, T b, T m) {
+   T res = 1;
+   while (b) {
+      if (b & 1) {
+         res = (res * a) % m;
+      }
+      a = (a * a) % m;
+      b = b >> 1;
+   }
+   return res % m;
+}
+void _IOE() {
+#ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+   freopen("error.txt", "w", stderr);
+#endif
+}
+// #define int long long
+class Solution {
+  public:
+   string minWindow(string s, string t) {
+      int n = s.size(), m = t.size();
+      string ans = "";
+      int i = 0, j = -1;
+      int mx = INT_MAX;
+      vector<int> h(150, 0);
+      for (char c : t) h[c]++;
+      vector<int> tmp(150, 0);
+      int l = 0, r = -1;
+      while (j < n) {
+         bool say = true;
+         for (int k = 0; k < 150; k++) {
+            if (h[k] > tmp[k]) {
+               say = false;
+               break;
+            }
+         }
+         if (!say) {
+            j++;
+            if (j < n)
+               tmp[s[j]]++;
+         } else {
+            if ((j - i) < mx) {
+               mx = j - i;
+               l = i, r = j;
+            }
+            tmp[s[i]]--;
+            i++;
+         }
+      }
+      for (int k = l; k <= r; k++) {
+         ans += s[k];
+      }
+      return ans;
+   }
+   void solution() {
+      string s, t;
+      cin >> s >> t;
+      cout << this->minWindow(s, t);
+   }
+};
+signed main() {
+   IOE;
+   _IOE();
+   int test_cases = 1;
+   // cin >> test_cases;
+   for (int i = 1; i <= test_cases; i++) {
+      Solution obj;
+      obj.solution();
+   }
+   return 0;
+}
